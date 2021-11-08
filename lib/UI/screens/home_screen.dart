294ce146037e9995b538e080c78514c6e692fc9a
sharon254 <sharonknyabuti@gmail.com>
2021-11-08 14:14:29 +0300

    initial commit

---
+++ lib/UI/screens/home_screen.dart
@@ -0,0 +1,308 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'detail_screen.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<ExerciseCategory> exerciseCategories = [];
+
+  getFoodCategories() {
+    // later work on getting data from the api here
+    exerciseCategories = [
+      ExerciseCategory("Arms", "assets/images/brachioplasty.png"),
+      ExerciseCategory("Butt", "assets/images/butt.png"),
+      ExerciseCategory("Back", "assets/images/camel.png"),
+      ExerciseCategory("Hips", "assets/images/legs.png"),
+      ExerciseCategory("Pilates", "assets/images/pilates.png"),
+      ExerciseCategory("Waist", "assets/images/waist.png"),
+      ExerciseCategory("arms!", "assets/images/weightlift.png"),
+      ExerciseCategory("arms2", "assets/images/hero.png"),
+    ];
+  }
+
+  @override
+  void initState() {
+    getFoodCategories();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Text(
+            'Training',
+            style: TextStyle(color: Colors.black),
+          ),
+        ),
+        backgroundColor: Colors.white,
+        elevation: 0,
+        actions: [
+          Icon(
+            Icons.arrow_back_ios,
+            size: 20,
+            color: Colors.black,
+          ),
+          SizedBox(
+            width: 10,
+          ),
+          Icon(
+            Icons.calendar_today,
+            size: 20,
+            color: Colors.black,
+          ),
+          SizedBox(
+            width: 10,
+          ),
+          Icon(
+            Icons.arrow_forward_ios,
+            size: 20,
+            color: Colors.black,
+          ),
+        ],
+      ),
+      body: SafeArea(
+        child: Container(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Text('Your program'),
+                    Expanded(
+                      child: Row(),
+                    ),
+                    Text('Details'),
+                    Icon(
+                      Icons.arrow_forward,
+                      size: 20,
+                      color: Colors.black,
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: 200,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(80),
+                        topLeft: Radius.circular(10),
+                        bottomLeft: Radius.circular(10),
+                        bottomRight: Radius.circular(10),
+                      ),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.6),
+                          spreadRadius: 5,
+                          blurRadius: 7,
+                          offset: Offset(0, 3),
+                        )
+                      ],
+                      gradient: LinearGradient(colors: [
+                        Colors.deepPurpleAccent,
+                        Colors.indigoAccent.withOpacity(0.8),
+                      ])),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Next Workout',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 15,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        Text(
+                          'Legs Toning',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 25,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                        Text(
+                          'and Glutes Workout',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 25,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                        Expanded(
+                          child: Container(),
+                        ),
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.access_time_rounded,
+                              color: Colors.white,
+                              size: 20,
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Text('60 min'),
+                            Expanded(
+                              child: Container(),
+                            ),
+                            ElevatedButton(
+                              child: Icon(
+                                Icons.play_arrow,
+                                size: 30,
+                                color: Colors.indigoAccent,
+                              ),
+                              onPressed: () {},
+                              style: ElevatedButton.styleFrom(
+                                  shape: CircleBorder(), primary: Colors.white),
+                            ),
+                            /*Icon(
+                              Icons.play_circle_fill,
+                              color: Colors.white,
+                              size: 40,
+                            ) */
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  height: 200,
+                  width: MediaQuery.of(context).size.width,
+                  child: Stack(
+                    children: [
+                      Container(
+                        height: 120,
+                        width: 370,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.indigoAccent.withOpacity(0.6),
+                              spreadRadius: 5,
+                              blurRadius: 7,
+                              offset: Offset(0, 3),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        height: 120,
+                        width: 200,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          image: DecorationImage(
+                              image: AssetImage('assets/images/yoga2.png')),
+                        ),
+                      ),
+                      Positioned(
+                        top: 20,
+                        left: 100,
+                        height: 250,
+                        width: 250,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            height: 200,
+                            width: MediaQuery.of(context).size.width,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: [
+                                Text(
+                                  'You are doing great! ',
+                                  style: TextStyle(
+                                      fontSize: 20, color: Colors.white),
+                                ),
+                                SizedBox(
+                                  height: 10,
+                                ),
+                                Text(
+                                  'Keep it up! ',
+                                  style: TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.w500,
+                                      color: Colors.white),
+                                ),
+                                SizedBox(
+                                  height: 10,
+                                ),
+                                Text(
+                                  'Stick to your plan',
+                                  style: TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.w500,
+                                      color: Colors.white),
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+
+                Container(
+                  child:Flexible(
+                    child: GridView.builder(
+                        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                            maxCrossAxisExtent: 200,
+                            childAspectRatio: 3 / 2,
+                            crossAxisSpacing: 20,
+                            mainAxisSpacing: 20),
+                        itemCount: exerciseCategories.length,
+                        itemBuilder: (BuildContext ctx, index) {
+                          return Container(
+                            alignment: Alignment.center,
+                            child: Text(exerciseCategories[index].name),
+
+                            decoration: BoxDecoration(
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.indigoAccent.withOpacity(0.3),
+                                  spreadRadius: 5,
+                                  blurRadius: 7,
+                                  offset: Offset(0, 3),
+                                ),
+                              ],
+                                image: DecorationImage(
+                                    image: AssetImage(exerciseCategories[index].image),
+
+
+                                ),
+                            ),
+                          );
+                        }),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ExerciseCategory {
+  final String name, image;
+
+  ExerciseCategory(this.name, this.image);
+}
